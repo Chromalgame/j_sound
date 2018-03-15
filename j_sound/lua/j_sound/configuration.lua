5f49a43837a281cfd1232fb577bdde9f7b77c45b
Jenni Gort <36727428+Jenni-Gort@users.noreply.github.com>
2018-03-15 21:31:28 +0100

    Ajout rank + context menu

--- j_sound/lua/j_sound/configuration.lua
+++ j_sound/lua/j_sound/configuration.lua
@@ -1,10 +1,16 @@
 --[[ Configuration ]]--
-j_sound.config.ChatCommand = "/sound" -- la commande a entrer dans le chat pour ouvrir le menu
-j_sound.config.Titre = "Sound Menu [F3]"
-j_sound.config.TitrePosition = 20 -- Positionnement vertical du titre
+j_sound.config.ContextMenu = false -- Ouvrir le menu avec le contexte menu a la place du ChatCommand
+if j_sound.config.ContextMenu == true then
+	j_sound.config.Titre = "Sound Menu"
+	j_sound.config.TitrePosition = 40 -- Positionnement vertical du titre
+else
+	j_sound.config.ChatCommand = "/sound" -- la commande a entrer dans le chat pour ouvrir le menu
+	j_sound.config.Titre = "Sound Menu [F3]"
+	j_sound.config.TitrePosition = 20 -- Positionnement vertical du titre
+end
 j_sound.config.CouleurTitre = Color(150, 150, 150, 255)
 j_sound.config.Hauteur = "400" -- Hauteur du menu
-j_sound.config.Position = "Gauche" -- "Droite" ou "Gauche"
+j_sound.config.Position = "Droite" -- "Droite" ou "Gauche"
 j_sound.config.Couleur = Color(30, 144, 255, 145) -- Couleur des lignes du menu
 j_sound.config.AntiSpam = 0.8 -- Secondes pour relancer un son
 j_sound.config.F3_Fix = false -- Mettre en true seulement si F3 ne fonctionne pas !!!
@@ -15,17 +21,29 @@ j_sound.config.WhiteListJobs = {TEAM_CITIZEN, TEAM_POLICE} -- a ignorer si vous 
 j_sound.add[1] = { -- unique id
 	sound_name = "Excuse me", -- nom du son
     sound_path = "vo/npc/female01/excuseme01.wav", -- chemin du son
-    allowed = function() return j_sound.allowed(LocalPlayer(), {TEAM_CITIZEN, TEAM_POLICE}) end -- Ajouter les jobs autoriser a utilisé le son 
+    allowed = function() return j_sound.allowed(LocalPlayer(), {TEAM_CITIZEN, TEAM_POLICE}, {"superadmin", "admin"}) end -- Ajouter les jobs/ranks autoriser a utilisé le son 
 }
-
+-- Exemple: Juste pour plusieurs job
 j_sound.add[2] = {
 	sound_name = "Finally",
     sound_path = "vo/npc/female01/finally.wav",
-    allowed = function() return j_sound.allowed(LocalPlayer(), {TEAM_CITIZEN}) end
+    allowed = function() return j_sound.allowed(LocalPlayer(), {TEAM_CITIZEN, TEAM_POLICE}, {}) end
 }
-
+-- Exemple: Juste pour un job
 j_sound.add[3] = {
 	sound_name = "Hacks",
     sound_path = "vo/npc/male01/hacks01.wav",
-    allowed = function() return j_sound.allowed(LocalPlayer(), {TEAM_POLICE}) end
+    allowed = function() return j_sound.allowed(LocalPlayer(), {TEAM_POLICE}, {}) end
+}
+-- Exemple: Juste pour un rank
+j_sound.add[4] = {
+	sound_name = "Get Down",
+    sound_path = "vo/npc/female01/getdown02.wav",
+    allowed = function() return j_sound.allowed(LocalPlayer(), {}, {"superadmin"}) end
+}
+-- Exemple: Juste pour plusieurs ranks
+j_sound.add[5] = {
+	sound_name = "Ok",
+    sound_path = "vo/npc/female01/ok02.wav",
+    allowed = function() return j_sound.allowed(LocalPlayer(), {}, {"superadmin", "admin"}) end
 }
