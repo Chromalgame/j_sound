2c379e417768d14d330dc6a820c82936f65fe237
Jenni Gort <36727428+Jenni-Gort@users.noreply.github.com>
2018-03-17 04:40:13 +0100

    Correction de petit bug + optimisation

--- j_sound/lua/autorun/j_sound_load.lua
+++ j_sound/lua/autorun/j_sound_load.lua
@@ -2,16 +2,17 @@ j_sound = {}
 j_sound.config = {}
 j_sound.add = {}
 j_sound.allowed = {}
+AddCSLuaFile()
 
 function j_sound.allowed(ply, team, rank)
-    if table.HasValue(team, ply:Team()) or table.HasValue(rank, ply:GetUserGroup()) then return true end
+    if team[ply:Team()] || rank[ply:GetUserGroup()] then return true end
 end
 
 if SERVER then
     util.AddNetworkString("j_sound_emit")
-    AddCSLuaFile("j_sound/cl_j_sound.lua")
     AddCSLuaFile("j_sound/configuration.lua")
     include("j_sound/configuration.lua")
+    AddCSLuaFile("j_sound/cl_j_sound.lua")
     include("j_sound/sv_j_sound.lua")
 else
     include("j_sound/configuration.lua")
