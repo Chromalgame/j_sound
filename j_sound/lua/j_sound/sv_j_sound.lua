b92a250afc3251e67e5367957ec5f57196ca9abd
Jenni Gort <36727428+Jenni-Gort@users.noreply.github.com>
2018-03-17 04:41:58 +0100

    Update sv_j_sound.lua

--- j_sound/lua/j_sound/sv_j_sound.lua
+++ j_sound/lua/j_sound/sv_j_sound.lua
@@ -1,6 +1,6 @@
 net.Receive("j_sound_emit", function(len, ply)
-    if ply.StopSpam and ply.StopSpam > CurTime() then return end
-    if not ply:Alive() or not ply:IsValid() then return end
+    if ply.StopSpam && ply.StopSpam > CurTime() then return end
+    if !ply:Alive() || !ply:IsValid() then return end
     local tableNumber = net.ReadUInt(32)
     ply:EmitSound(j_sound.add[tableNumber].sound_path, 80, 100)
     ply.StopSpam = CurTime() + j_sound.config.AntiSpam
