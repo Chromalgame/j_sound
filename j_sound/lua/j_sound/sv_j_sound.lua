429118c78d901bc13dd79b4f5d9069d5c4018dea
Jenni Gort <36727428+Jenni-Gort@users.noreply.github.com>
2018-03-20 08:20:59 +0100

    Update sv_j_sound.lua

--- j_sound/lua/j_sound/sv_j_sound.lua
+++ j_sound/lua/j_sound/sv_j_sound.lua
@@ -1,7 +1,16 @@
 net.Receive("j_sound_emit", function(len, ply)
-    if ply.StopSpam && ply.StopSpam > CurTime() then return end
-    if !ply:Alive() || !ply:IsValid() then return end
-    local tableNumber = net.ReadUInt(32)
+	local tableNumber = net.ReadUInt(32)
+    if (!ply:Alive() || !ply:IsValid()) || (ply.StopSpam && ply.StopSpam > CurTime()) then return end
     ply:EmitSound(j_sound.add[tableNumber].sound_path, 80, 100)
     ply.StopSpam = CurTime() + j_sound.config.AntiSpam
 end)
+
+if j_sound.config.MethodeOuverture == 3 then
+	hook.Add( "PlayerButtonDown", "SoundMenu.OpenClose.Touche", function(ply, btn) 
+		if !ply:Alive() || !ply:IsValid() then return end
+		if ( btn == j_sound.config.Touche ) then
+			net.Start("j_sound_open_close")
+			net.Send(ply)
+		end
+	end)
+end
