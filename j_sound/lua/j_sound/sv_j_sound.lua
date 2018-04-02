83d7f2cd1a2b5676f2a22f021d787694b20dd305
Jenni Gort <36727428+Jenni-Gort@users.noreply.github.com>
2018-04-02 15:57:28 +0200

    Update sv_j_sound.lua

--- j_sound/lua/j_sound/sv_j_sound.lua
+++ j_sound/lua/j_sound/sv_j_sound.lua
@@ -1,25 +1,25 @@
 net.Receive("j_sound_emit", function(len, ply)
-	local tableNumber = net.ReadUInt(32)
+    local tableNumber = net.ReadUInt(32)
     if (!ply:Alive() || !ply:IsValid()) || (ply.StopSpam && ply.StopSpam > CurTime()) then return end
     ply:EmitSound(j_sound.add[tableNumber].sound_path, 80, 100)
     ply.StopSpam = CurTime() + j_sound.config.AntiSpam
 end)
 
 net.Receive("j_sound_playsound", function(len, ply)
-	local tableNumber = net.ReadUInt(32)
-	if (!ply:Alive() || !ply:IsValid()) || (ply.StopSpam && ply.StopSpam > CurTime()) then return end
-	net.Start("j_sound_playsound")
-	net.WriteUInt(tableNumber, 32)
-	net.Broadcast()
+    local tableNumber = net.ReadUInt(32)
+    if (!ply:Alive() || !ply:IsValid()) || (ply.StopSpam && ply.StopSpam > CurTime()) then return end
+    net.Start("j_sound_playsound")
+    net.WriteUInt(tableNumber, 32)
+    net.Broadcast()
     ply.StopSpam = CurTime() + j_sound.config.AntiSpam
 end)
 
 if j_sound.config.MethodeOuverture == 3 then
-	hook.Add( "PlayerButtonDown", "SoundMenu.OpenClose.Touche", function(ply, btn) 
-		if !ply:Alive() || !ply:IsValid() then return end
-		if ( btn == j_sound.config.Touche ) then
-			net.Start("j_sound_open_close")
-			net.Send(ply)
-		end
-	end)
+    hook.Add( "PlayerButtonDown", "SoundMenu.OpenClose.Touche", function(ply, btn) 
+        if !ply:Alive() || !ply:IsValid() then return end
+        if ( btn == j_sound.config.Touche ) then
+            net.Start("j_sound_open_close")
+            net.Send(ply)
+        end
+    end)
 end
